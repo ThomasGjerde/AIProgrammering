94cd0ae8e343a062e46286fb67ef895cf296cbb9
Thomas Gjerde <thomasgjerde90@gmail.com>
2014-09-09 16:34:52 +0200

    Adds Board logic and input reading

--- src/aiprog/main/Main.java
+++ src/aiprog/main/Main.java
@@ -1,13 +1,27 @@
 package aiprog.main;
 
+import java.io.IOException;
+
+import aiprog.model.Board;
+
 public class Main {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Test");
-		System.out.println("HEI DUDE!");
+		try {
+			Board board = new Board("C:\\board.txt");
+			System.out.println("SizeX: " + board.sizeX);
+			System.out.println("SizeY: " + board.sizeY);
+			System.out.println("StartX: " + board.startX);
+			System.out.println("StartY: " + board.startY);
+			System.out.println("EndX: " + board.endX);
+			System.out.println("EndY: " + board.endY);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
---
+++ src/aiprog/model/Board.java
@@ -0,0 +1,62 @@
+package aiprog.model;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Board {
+	public int sizeX;
+	public int sizeY;
+	public int startX;
+	public int startY;
+	public int endX;
+	public int endY;
+	public Node[][] boardArray;
+	
+	public Board(String path) throws IOException{
+		ArrayList<String> input = getInputFromFile(path);
+		if(input.size() >= 2){
+			setBoardSize(parseLine(input.get(0)));
+			setStartAndEnd(parseLine(input.get(1)));
+			generateBoard();
+		}
+	}
+	private void generateBoard(){
+		boardArray = new Node[sizeX][sizeY];
+		for(int i = 0; i < sizeX; i++){
+			for(int j = 0; j < sizeY; j++)
+			{
+				boardArray[i][j] = new Node();
+			}
+		}
+	}
+	private void setBoardSize(ArrayList<Integer> input){
+		sizeX = input.get(0);
+		sizeY = input.get(1);
+	}
+	private void setStartAndEnd(ArrayList<Integer> input){
+		startX = input.get(0);
+		startY = input.get(1);
+		endX = input.get(2);
+		endY = input.get(3);
+	}
+	private ArrayList<Integer> parseLine(String input){
+		String tempArray[] = input.split(",");
+		ArrayList<Integer> returnArray = new ArrayList<Integer>();
+		for(int i = 0; i < tempArray.length; i++){
+			returnArray.add(Integer.parseInt(tempArray[i]));
+			System.out.println(tempArray[i]);
+		}
+		return returnArray;
+	}
+	private ArrayList<String> getInputFromFile(String path) throws IOException{
+		BufferedReader br = new BufferedReader(new FileReader(new File(path)));
+		ArrayList<String> returnArray = new ArrayList<String>();
+		String line;
+	    while ((line = br.readLine()) != null) {
+	        returnArray.add(line.replace("(", "").replace(")","").replace(" ", ","));
+	    }
+	    br.close();
+	    return returnArray;
+	}
+
+}
