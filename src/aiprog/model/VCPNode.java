a3975336620b245813065c34563ab6378b9055d8
magnulef <lefdal.magnus@gmail.com>
2014-09-29 20:53:51 +0200

    color stuff, that might just maybe work

--- src/aiprog/model/VCPNode.java
+++ src/aiprog/model/VCPNode.java
@@ -1,17 +1,48 @@
 package aiprog.model;
 
+import java.awt.Color;
 import java.util.ArrayList;
 
 public class VCPNode extends CSPNode{
-
-	public VCPNode(Point position) {
+	
+	public VCPNode(Point position, int k) {
 		super(position);
+		standardColors(k);
 	}
 
 	@Override
 	public ArrayList<Node> getUnoccupiedChildren() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	public Color getColor(){
+		Color color = new Color(nodeValue);
+		return color;
+	}
+	
+	public void setColor(Color color){
+		nodeValue = color.getRGB();
+	}
+	
+	public void standardColors(int k){
+		ArrayList<Color> standardColors = new ArrayList<Color>();
+		standardColors.add(Color.RED);
+		standardColors.add(Color.BLUE);
+		standardColors.add(Color.CYAN);
+		standardColors.add(Color.DARK_GRAY);
+		standardColors.add(Color.YELLOW);
+		standardColors.add(Color.LIGHT_GRAY);
+		standardColors.add(Color.GREEN);
+		standardColors.add(Color.PINK);
+		standardColors.add(Color.MAGENTA);
+		standardColors.add(Color.ORANGE);
+		
+		for(int i=0; i<k; i++){
+			domain.add(standardColors.get(i).getRGB());
+		}
+		
+		
 	}
 
 }
