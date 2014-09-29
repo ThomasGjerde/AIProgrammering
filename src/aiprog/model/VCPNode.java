fc767755efb6866297121263c063e84eadcb5348
Thomas Gjerde <thomasgjerde90@gmail.com>
2014-09-29 20:32:21 +0200

    New node

---
+++ src/aiprog/model/VCPNode.java
@@ -0,0 +1,17 @@
+package aiprog.model;
+
+import java.util.ArrayList;
+
+public class VCPNode extends CSPNode{
+
+	public VCPNode(Point position) {
+		super(position);
+	}
+
+	@Override
+	public ArrayList<Node> getUnoccupiedChildren() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
