020740799e01e37958e2104fa747815f14699f38
magnulef <lefdal.magnus@gmail.com>
2014-09-29 20:32:49 +0200

    CSP doin work

--- src/aiprog/model/CSP.java
+++ src/aiprog/model/CSP.java
@@ -1,5 +1,7 @@
 package aiprog.model;
 
+import java.util.ArrayList;
+
 public abstract class CSP extends AStarGAC{
 
 	public CSP(Node startNode) {
@@ -12,8 +14,36 @@ public abstract class CSP extends AStarGAC{
 	}
 	@Override
 	protected void setHeuristic(Node node) {
+		ArrayList<CSPNode> tempCSPList = ((GACNode)node).getCSPList();
+		int heuristic = 0;
+		for(int i=0; i<tempCSPList.size(); i++){
+			
+		}
 		
+		//Legger sammen alle domenene
+		for(int j=0; j<tempCSPList.size(); j++){
+			heuristic += tempCSPList.get(j).getDomain().size();
+		}
+		//abcdefghijklmnop
+		//reduction
+		for(int k=0; k<tempCSPList.size(); k++){
+			//VCPNode tempVCPNode = (VCPNode)tempCSPList.get(k);
+		}
 		
+		//
+		/*
+		public void reduction(ColorNode node){
+			for(int i=0; i<node.getChildren().size(); i++){
+				ColorNode midChild = (ColorNode)node.getChildren().get(i);
+				if(node.getColor() != null){
+					if(midChild.getDomain().contains(node.getColor())){
+						midChild.reduseDomain(node.getColor());
+					}
+				}
+			}
+			check();
+		}
+		*/
 	}
 	
 
