c37d184ed7afc9beb67b925a4ff4b7271f4b4dab
magnulef <lefdal.magnus@gmail.com>
2014-10-21 22:02:27 +0200

    bare la til 2 andre classer vi trenger

---
+++ src/aiprog/flowfree/FlowFree.java
@@ -0,0 +1,25 @@
+package aiprog.flowfree;
+
+import aiprog.csp.CSP;
+import aiprog.model.Node;
+
+public class FlowFree extends CSP {
+
+	public FlowFree(Node startNode) {
+		super(startNode);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected boolean checkVictory() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected void updateGui() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/aiprog/model/FFStateNode.java
@@ -0,0 +1,17 @@
+package aiprog.model;
+
+public class FFStateNode {
+	
+	/*
+	 * Er ikke sikker på hva vi faktisk trenger her
+	 * Siden vi skal bruke aStar i cspen trenger vi ikke
+	 * parent/children osv
+	 * 
+	 * Vi trenger en måte og traversere noder på
+	 * Egentlig kunne denne her kun ha inneholdt et array av noder
+	 */
+	
+	public FFStateNode(){
+		
+	}
+}
