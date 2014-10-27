336025918fd5e5772df777e4501221be0d123ac0
magnulef <lefdal.magnus@gmail.com>
2014-10-27 15:35:12 +0100

    findcommon og reduction

--- src/aiprog/nonogram/NonoGram.java
+++ src/aiprog/nonogram/NonoGram.java
@@ -1,13 +1,18 @@
 package aiprog.nonogram;
 
+import java.util.ArrayList;
+
 import aiprog.model.NNColRow;
+import aiprog.model.NNStateNode;
 import aiprog.model.Node;
 import aiprog.search.AStar;
 
 public class NonoGram extends AStar{
 
+	NNStateNode currentState;
 	public NonoGram(Node startNode) {
 		super(startNode);
+		currentState = (NNStateNode)startNode;
 		// TODO Auto-generated constructor stub
 	}
 	
@@ -16,8 +21,37 @@ public class NonoGram extends AStar{
 		
 	}
 	
+	public void initReduction(NNStateNode state){
+		
+	}
+	
+	public ArrayList<Integer> findCommon(ArrayList<ArrayList<Boolean>> domain){
+		
+		ArrayList<Integer> intArray = new ArrayList<Integer>();
+		for(int i=0; i<domain.size(); i++){
+			if(domain.get(0).get(0) == true){
+				intArray.add(1);
+			}else{
+				intArray.add(0);
+			}
+		}
+		
+		for(int j=0; j<domain.size(); j++){
+			for(int k=0; k<domain.get(j).size(); k++){
+				if(intArray.get(k) == 1 && domain.get(j).get(k) != true){
+					intArray.set(k, 3);
+				}
+				if(intArray.get(k) == 0 && domain.get(j).get(k) != false){
+					intArray.set(k, 3);
+				}
+			}
+		}
+		return intArray;
+		
+	}
+	
 	//Skal redusere domenene til vær rowcol
-	//Skal kanskje ta inn 2 rowcol objecter, ala en row og en col
+	//Skal kanskje ta inn ett rowcol object og redusere domenet til denne basert på en value
 	public void reduction(NNColRow obj, int pos, boolean value){
 		for(int i=0; i<obj.getDomain().size(); i++){
 			if(obj.getDomain().get(i).get(pos) != value){
