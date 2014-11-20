be6511183a219fcd75458e46e14242421367b0fc
Thomas Gjerde <thomasgjerde90@gmail.com>
2014-11-20 19:42:42 +0100

    Adds random number

--- src/aiprog/twentyfortyeigth/TFE.java
+++ src/aiprog/twentyfortyeigth/TFE.java
@@ -12,6 +12,7 @@ public class TFE {
 			MinMax minMax = new MinMax(board);
 			Move bestMove = minMax.search(-10000, 10000, 3);
 			board.move(bestMove.getDirection());
+			board.generateRandomNumber();
 			graphics.animateSetBoard(board, bestMove.getDirection());
 		}
 		//Put Win || Fail check here
