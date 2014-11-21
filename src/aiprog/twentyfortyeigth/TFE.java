8192508361dcfde0877a56bf17c95ea46fc4008d
Thomas Gjerde <thomasgjerde90@gmail.com>
2014-11-21 18:11:52 +0100

    Test loop

--- src/aiprog/twentyfortyeigth/TFE.java
+++ src/aiprog/twentyfortyeigth/TFE.java
@@ -5,24 +5,32 @@ import aiprog.model.Move;
 
 public class TFE {
 	public TFE(){
-		TfeBoard board = new TfeBoard(true,2048);
-		TFEGraphics graphics = new TFEGraphics();
-		graphics.setBoard(board);
-		while(!board.hasFailed() && !board.victoryCheck()) {//!board.isWin && board.hasFailed
-			//System.out.println("While loop");
-			MinMax minMax = new MinMax(board);
-			Move bestMove = minMax.search(-10000, 10000, 2);
-			board.move(bestMove.getDirection());
-			board.generateRandomNumber();
-			System.out.println(bestMove.getDirection());
-			graphics.animateSetBoard(board, bestMove.getDirection());
-		}
-		if(board.hasFailed()){
-			System.out.println("Failed");
+		int wins = 0;
+		int fails = 0;
+		for(int i = 0; i < 10; i++){
+			TfeBoard board = new TfeBoard(true,2048);
+			TFEGraphics graphics = new TFEGraphics();
 			graphics.setBoard(board);
-		}else{
-			System.out.println("Victory");
-			graphics.setBoard(board);
+			while(!board.hasFailed() && !board.victoryCheck()) {
+				MinMax minMax = new MinMax(board);
+				Move bestMove = minMax.search(-10000, 10000, 2);
+				board.move(bestMove.getDirection());
+				board.generateRandomNumber();
+				//System.out.println(bestMove.getDirection());
+				//graphics.animateSetBoard(board, bestMove.getDirection());
+			}	
+			if(board.hasFailed()){
+				System.out.println("Failed");
+				graphics.setBoard(board);
+				fails++;
+			}else{
+				System.out.println("Victory");
+				graphics.setBoard(board);
+				wins++;
+			}
 		}
+		System.out.println("Wins: " + wins);
+		System.out.println("Fails: " + fails);
+		
 	}
 }
